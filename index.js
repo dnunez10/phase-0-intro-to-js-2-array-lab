6a45256d7f2c45c3940d402f1ef9f2e671b42e61
Diana <diana_nunez10@yahoo.com>
2021-02-18 14:46:57 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+var cats = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendCat(appendCat){
+    cats.push(appendCat);
+    return cats;
+  }
+  function destructivelyPrependCat(prependCat){
+      cats.unshift(prependCat);
+      return cats;
+  }
+  function destructivelyRemoveLastCat(){
+      cats.pop();
+      return cats;
+  }
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+function appendCat(appendCats){
+    return [...cats, appendCats];
+}
+function prependCat(prependCats){
+    return [prependCats, ...cats];
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+  }
+function removeFirstCat(){
+    return cats.slice(1);
+  }
